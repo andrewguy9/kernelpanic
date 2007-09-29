2e0205eadf13dc0f552c4371d7613baff776a59c
andrewguy9 <andrewguy9>
2007-09-29 22:55:40 +0000

    Wrote rules for utilities.

---
+++ utils/rules.mk
@@ -0,0 +1,10 @@
+###################
+#utilities
+utils/$(TARGET)_linkedlist.o: utils/linkedlist.c utils/linkedlist.h
+
+utils/$(TARGET)_heap.o: utils/heap.c utils/heap.h
+
+###################3
+#tests
+utils/linktest.o: utils/linktest.c
+	
