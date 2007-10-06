fedcd5dfa608fd71b3e130786a4b90055874dca9
andrewguy9 <andrewguy9>
2007-10-06 05:27:42 +0000

    Redoing scheduler.

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -6,6 +6,32 @@
 
 #define SCHEDULER_QUANTUM 10
 
+/*
+ * Scheduler Unit:
+ * The Scheduler unit has three tasks:
+ * 1) Provide a thread scheduler which
+ * picks when threads run.
+ * 2) Provide mechanisms to start and stop
+ * threads.
+ * 3) Provide mechanism to prevent the sceduler
+ * from splitting thread level atomic
+ * operations.
+ *
+ * Scheduling next thread:
+ * The actual Scheduling occurs in the Schedule
+ * function. We must make sure that the Schedule 
+ * function does pick a new thread while the 
+ * system is in a thread critical section. 
+ * We enforce this rule using a mutex, 
+ * threads entering a critical section
+ * aquire the mutex, and then release it
+ * at the end of their critical section. 
+ *
+ * Resuming and Blocking threads:
+ * Resuming threads will require locking the waiting
+ * thread 
+ */
+
 struct THREAD * ActiveThread;
 struct THREAD * NextThread;
 struct HEAP ThreadHeap;
@@ -16,9 +42,7 @@ struct MUTEX SchedulerLock;
 void SchedulerStartCritical( )
 {
 	BOOL worked = MutexLock( & SchedulerLock );
-	ASSERT( worked, 
-			"Scheduler was not enabled at start of \
-			critical section.");
+	ASSERT( worked, "Could not start critical section" );
 }
 
 void SchedulerEndCritical()
@@ -26,69 +50,31 @@ void SchedulerEndCritical()
 	MutexUnlock( & SchedulerLock );
 }
 
+void SchedulerForceSwitch()
+{
+	//TODO
+}
+
 void SchedulerResumeThread( struct THREAD * thread )
 {
-	ASSERT( thread != NULL, "Thread cannot be null" );
-
-	SchedulerStartCritical();
-
-	ASSERT( thread->State == THREAD_STATE_BLOCKED,
-			"Only activate blocked threads" );
-	thread->State = THREAD_STATE_RUNNING;
-	HeapAdd( (struct WEIGHTED_LINK *) thread, &ThreadHeap );
-	SchedulerEndCritical();
+	//TODO
 }
 
 void SchedulerBlockThread( )
 {
-	ASSERT( ActiveThread != NULL, "thread cannot be null" );
-	ASSERT( ActiveThread->State == THREAD_STATE_RUNNING, 
-			"Cant stop a thread that isn't running." );
-
-	SchedulerStartCritical();
-	ActiveThread->State = THREAD_STATE_BLOCKED;
-	//TODO: Force a Scheduling event.
-	SchedulerEndCritical();
+	//TODO
 }
 
 void Schedule( ) 
 {
-	if( ! MutexLock( &SchedulerLock ) )
-	{
-		//TODO so, maybe we should try again, since we failed...
-		return;
-	}
-
-	//If there is an active thread, add him back
-	//into the heap with some penalty.
-	if( ActiveThread != NULL )
-	{
-		//Apply penalty.
-		ActiveThread->Link.WeightedLink.Weight += 
-			ActiveThread->Priority;
-		//Add to queue
-		HeapAdd( (struct WEIGHTED_LINK *) ActiveThread,
-			  &ThreadHeap );
-	}
-	ActiveThread = NULL;
-
-	//Retrive the next thread off the heap
-	NextThread = (struct THREAD *) HeapPop( & ThreadHeap );
-
-	//Schedule Next Scheduling event.
-	TimerRegisterASR( & ScheduleTimer,
-		   	SCHEDULER_QUANTUM, 
-			Schedule );
-
-	MutexUnlock( &SchedulerLock );
-
-	return;
+	//TODO
 }
 
 void SchedulerInit()
 {
 	//Initialize ActiveThread
-	ActiveThread = NULL;
+	ActiveThread = NULL;//TODO Cannot be null, ever.
+	NextThread = NULL;
 	//Initialize heap
 	HeapInit( & ThreadHeap );
 	//Initialize the timer
