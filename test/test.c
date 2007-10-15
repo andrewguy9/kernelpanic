f8e4860d1075aea5d75ae0bb290ee20ff920379c
andrewguy9 <andrewguy9>
2007-10-15 04:55:54 +0000

    Added timers test, commented out thread tests.

--- test/test.c
+++ test/test.c
@@ -1,46 +1,89 @@
 #include"../kernel/startup.h"
 #include"../kernel/scheduler.h"
 #include"../kernel/semaphore.h"
+#include"../kernel/timer.h"
 
-struct THREAD TestThreadIncrement;
-char TestThreadStackIncrement[500];
+//Tests of the Timer subsystem.
+struct TIMER FrequentTimer;
+COUNT FrequentCount;
+void FrequentHandler( )
+{
+	FrequentCount++;
+	TimerRegister( 
+			&FrequentTimer,
+			2,
+			FrequentHandler );
+}
 
-struct THREAD TestThreadDivide;
-char TestThreadStackDivide[500];
+struct TIMER SeldomTimer;
+COUNT SeldomCount;
+void SeldomHandler( )
+{
+	SeldomCount++;
+	TimerRegister(
+			&SeldomTimer,
+			3,
+			SeldomHandler );
+}
 
+//Tests of the multithreading system
 struct SEMAPHORE ValueLock;
 unsigned int Value = 0;
 
+struct THREAD TestThreadIncrement;
+char TestThreadStackIncrement[500];
 void TestMainIncrement()
 {
-	int a = 0;
+	unsigned long int a = 0;
 	while( 1 )
 	{
-		SemaphoreLock( & ValueLock );
-		for( a=0; a<0xffff; a++);
+		//SemaphoreLock( & ValueLock );
+		for( a=0; a<0xffffffff; a++);
 		Value++;
-		for( a=0; a<0xffff; a++);
-		SemaphoreUnlock( & ValueLock );
+		for( a=0; a<0xffffffff; a++);
+		//SemaphoreUnlock( & ValueLock );
 
 	}
 }
 
+struct THREAD TestThreadDivide;
+char TestThreadStackDivide[500];
 void TestMainDivide()
 {
-	int a = 0;
+	unsigned long int a = 0;
 	while( 1 )
 	{
-		SemaphoreLock( & ValueLock );
-		for( a=0; a<0xffff; a++);
+		//SemaphoreLock( & ValueLock );
+		for( a=0; a<0xffffffff; a++);
 		Value/2;
-		for( a=0; a<0xffff; a++);
-		SemaphoreUnlock( & ValueLock );
+		for( a=0; a<0xffffffff; a++);
+		//SemaphoreUnlock( & ValueLock );
 	}
 }
+
+
+
 
 int main()
 {
+	//Initialize the kernel structures.
 	KernelInit();
+
+	//Initialize timers.
+	FrequentCount = 0;
+	TimerRegister( 
+			&FrequentTimer,
+			2,
+			FrequentHandler );
+	SeldomCount = 0;
+	TimerRegister( 
+			&SeldomTimer,
+			3,
+			SeldomHandler );
+			
+
+	//Initialize Threads
+	/*
 	SemaphoreInit( &ValueLock, 1 );
 	SchedulerCreateThread(
 			&TestThreadIncrement,
@@ -54,6 +97,8 @@ int main()
 			TestThreadStackDivide,
 			500,
 			TestMainDivide);
+			*/
+
 	KernelStart();
 	return 0;
 }
