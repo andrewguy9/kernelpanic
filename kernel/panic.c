efa90d3684d2d6286e5ccd7968fa3060a320c5ca
andrewguy9 <andrewguy9>
2007-10-27 03:00:00 +0000

    Added error codes for new tests and HalEnableinterrupts so we can resume on error at right Interupt level.

--- kernel/panic.c
+++ kernel/panic.c
@@ -6,6 +6,8 @@ void GeneralPanic( enum ERROR_CODE error )
 	volatile char failed = 1;
 	HalDisableInterrupts();
 	while(failed);
+	//allows for the machine to contunue in debug.
+	HalEnableInterrupts();
 }
 
 void KernelPanic( enum ERROR_CODE error )
@@ -13,4 +15,6 @@ void KernelPanic( enum ERROR_CODE error )
 	volatile char failed = 1;
 	HalDisableInterrupts();
 	while(failed);
+	//Allow machine to continue on error
+	HalEnableInterrupts();
 }
