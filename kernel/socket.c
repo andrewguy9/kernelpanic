2f6591e44fbd025b16f360823729562c198b0f5f
andrewguy9 <andrewguy9>
2007-11-25 22:24:05 +0000

    Updated socket to use semaphores to do locking, and avoid deadlock.

--- kernel/socket.c
+++ kernel/socket.c
@@ -1,39 +1,62 @@
 #include"socket.h"
 
+/*
+ * Unit Description:
+ * Defines a Socket structure for threads.
+ */
+
 void PipeInit( char * buff, COUNT size, struct PIPE * pipe )
 {
 	SemaphoreInit( pipe->Mutex, 1 );
-	SemaphoreInit( pipe->EmptyLock, 0 );
-	SemaphoreInit( pipe->FullLock, 1 );
+	SemaphoreInit( pipe->EmptyLock, 0 ); //>0 when readable, <=0 when empty
+	SemaphoreInit( pipe->FullLock, 1 );//>0 when writable, <= when full
 	RingBufferInit( buff, size, pipe->Ring );
 }
 
 COUNT PipeRead( char * buff, COUNT size, struct PIPE * pipe )
 {
 	BOOL wasFull;
+	BOOL dataLeft;
 	COUNT read;
 
+	//Acquire locks
 	SemaphoreDown( lock->EmptyLock );
 	SemaphoreDown( lock->Mutex);
+	//Perform read
 	wasFull = RingBufferIsFull( pipe->Ring ); 
 	read = RingBufferRead( buff, size, pipe->Ring );
+	dataLeft = ! RingBufferIsEmpty( pipe->Ring );
+	//Release locks
 	SemaphoreUp( lock->Mutex );
-	if( wasFull )
+	if( dataLeft )
+		SemaphoreUp( pipe->EmptyLock );
+	if( wasFull && read > 0)
 		SemaphoreUp( pipe->FullLock );
+	//Return result
 	return read;
 }
 
 COUNT PipeWrite( char * buff, COUNT size, struct PIPE * pipe )
 {
 	BOOL wasEmtpy;
+	BOOL spaceLeft;
 	COUNT write;
+
+	//Acquire locks
 	SemaphoreDown( pipe->FullLock );
 	SemaphoreDown( pipe->Mutex );
+	//Perform write
 	wasEmpty = RingBufferIsEmpty( pipe->Ring );
 	write = RingBufferWrite( buff, size, pipe->Ring );
+	spaceLeft = RingBufferIsFull( pipe->Ring );
+	//Release locks
 	SemaphoreUp( pipe->Mutex );
-	if( wasEmpty )
+	if( spaceLeft )
+		SemaphoreUp( pipe->FullLock );
+	if( wasEmpty && write > 0 )
 		SemaphoreUp( pipe->EmptyLock );
+	//Return result
+	return write;
 }
 
 void SocketInit( struct PIPE * readPipe, struct PIPE * writePipe, struct SOCKET * socket )
