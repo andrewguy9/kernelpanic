6b8eacda7680db9ed227973e6b5db9caa43c8d5e
andrewguy9 <andrewguy9>
2007-12-21 08:22:41 +0000

    Updated rules for new bham unit.

--- utils/rules.mk
+++ utils/rules.mk
@@ -8,6 +8,8 @@ utils/$(TARGET)_ringbuffer.o: utils/ringbuffer.c utils/ringbuffer.h
 
 utils/$(TARGET)_flags.o: utils/flags.c utils/flags.h
 
+utils/$(TARGET)_bham.o: utils/bham.c utils/bham.h
+
 ###################
 #tests
 utils/linktest.o: utils/linktest.c 
@@ -15,3 +17,5 @@ utils/linktest.o: utils/linktest.c
 utils/ringtest.o: utils/ringtest.c
 
 utils/flagtest.o: utils/flagtest.c
+
+utils/bhamtest.o: utils/bhamtest.c
