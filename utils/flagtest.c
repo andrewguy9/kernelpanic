ae08cfd891dc7d5443a4f45d8ee9e54a4ba75c57
andrewguy9 <andrewguy9>
2007-12-21 08:25:01 +0000

    update to flagtest to not use characters because after 15 years of c development, I still cant remember how to use scanf effectively.

--- utils/flagtest.c
+++ utils/flagtest.c
@@ -22,37 +22,39 @@ void PrintFlags( FLAG_WORD flags[], COUNT numFlags )
 int main()
 {
 	COUNT numFlags;
-	char input;
+	INDEX input;
 	COUNT inputNum;
 	FLAG_WORD * flags;
 
 	printf("Enter a number of flags\n");
 	scanf("%d", &numFlags);
 
+	printf("Need %d bytes\n", FlagSize( numFlags ) * sizeof( FLAG_WORD ) );
+
 	flags = malloc( FlagSize( numFlags ) * sizeof( FLAG_WORD ) );
 	
 	do
 	{
 		PrintFlags( flags, numFlags );
-		printf("Set, Clear, Quit\n");
-		scanf("%c\n", & input );
+		printf("1 Set, 2 Clear, 3 Quit\n");
+		scanf("%d\n", & input );
 		switch( input )
 		{
-			case 's':
+			case 1:
 				printf("what index?");
 				scanf("%d",&inputNum);
 				FlagOn( flags, inputNum );
 				break;
-			case 'c':
+			case 2:
 				printf("what index?");
 				scanf("%d",&inputNum);
 				FlagOff( flags, inputNum );
 				break;
-			case 'q':
+			case 3:
 				printf("have a good day");
 				break;
 			default:
 				continue;
 		}
-	}while( input != 'q' );
+	}while( input != 3 );
 }
