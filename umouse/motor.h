76b0ea4d2ae64ee825a2bc972701045c0bd659ca
Laine Walker-Avina <ceriand@Glamdring.(none)>
2008-02-07 23:28:33 -0800

    added motor stubs and make lines

---
+++ umouse/motor.h
@@ -0,0 +1,12 @@
+#pragma once
+
+void motor_init(void);
+
+enum {
+	MOTOR_LEFT = 0,
+	MOTOR_RIGHT = 1
+};
+enum {
+	MOTOR_V = 0,
+	MOTOR_P = 1
+};
--- umouse/rules.mk
+++ umouse/rules.mk
@@ -3,4 +3,6 @@ umouse/$(TARGET)_map.o: umouse/map.c umouse/map.h
 
 umouse/maptest.o: umouse/maptest.c
 
-
+#motor control
+umouse/$(TARGET)_motor.o: umouse/motor.c umouse/motor.h
+umouse/motortest.o: umouse/motortest.o
