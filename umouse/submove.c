926a97ef6c5e7220d1e880ea83a43fbd8e62c4ac
Andrew Thomson <andrewguy9@andrewguy9-laptop.(none)>
2008-02-19 01:25:52 -0800

    cleaned up prints

--- umouse/submove.c
+++ umouse/submove.c
@@ -33,27 +33,27 @@ void SubMoveApply(INDEX * x, INDEX * y, enum DIRECTION * dir, enum SUB_MOVE move
 		case SUB_MOVE_FORWARD_WHOLE:
 			SubMoveTranslate(x,y,*dir,2);
 			*translated = TRUE;
-			printf("forward whole = %d,%d,%d\n",*x,*y,*dir);
+			//printf("forward whole = %d,%d,%d\n",*x,*y,*dir);
 			break;
 		case SUB_MOVE_FORWARD_HALF:
 			*translated = TRUE;
 			SubMoveTranslate(x,y,*dir,1);
-			printf("forward half = %d,%d,%d\n",*x,*y,*dir);
+			//printf("forward half = %d,%d,%d\n",*x,*y,*dir);
 			break;
 		case SUB_MOVE_TURN_RIGHT:
 			*rotated = TRUE;
 			SubMoveRotate(dir, RIGHT);
-			printf("turn right = %d,%d,%d\n",*x,*y,*dir);
+			//printf("turn right = %d,%d,%d\n",*x,*y,*dir);
 			break;
 		case SUB_MOVE_TURN_LEFT:
 			*rotated = TRUE;
 			SubMoveRotate(dir, LEFT);
-			printf("turn left = %d,%d,%d\n",*x,*y,*dir);
+			//printf("turn left = %d,%d,%d\n",*x,*y,*dir);
 			break;
 		case SUB_MOVE_TURN_AROUND:
 			*rotated = TRUE;
 			SubMoveRotate(dir, BACK );
-			printf("turn around = %d,%d,%d\n",*x,*y,*dir);
+			//printf("turn around = %d,%d,%d\n",*x,*y,*dir);
 			break;
 		case SUB_MOVE_INTEGRATE_RIGHT:
 			*translated = TRUE;
@@ -61,7 +61,7 @@ void SubMoveApply(INDEX * x, INDEX * y, enum DIRECTION * dir, enum SUB_MOVE move
 			SubMoveTranslate(x,y,*dir,1);
 			SubMoveRotate(dir, RIGHT);
 			SubMoveTranslate( x, y, *dir, 1 );
-			printf("integrate right = %d,%d,%d\n",*x,*y,*dir);
+			//printf("integrate right = %d,%d,%d\n",*x,*y,*dir);
 			break;
 		case SUB_MOVE_INTEGRATE_LEFT:
 			*translated = TRUE;
@@ -69,7 +69,7 @@ void SubMoveApply(INDEX * x, INDEX * y, enum DIRECTION * dir, enum SUB_MOVE move
 			SubMoveTranslate(x,y,*dir,1);
 			SubMoveRotate(dir, LEFT);
 			SubMoveTranslate( x, y, *dir, 1 );
-			printf("integrate left = %d,%d,%d\n",*x,*y,*dir);
+			//printf("integrate left = %d,%d,%d\n",*x,*y,*dir);
 			break;
 	}
 }
