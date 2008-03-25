909ec2ad2dd0d609d45fb0ccb3ca6dfbdfefa56d
Andrew Thomson <andrewguy9@andrewguy9-laptop.(none)>
2008-03-24 22:48:52 -0700

    updates to asserts. tracking down bugzzzz.

--- kernel/locking.c
+++ kernel/locking.c
@@ -4,21 +4,42 @@
 
 void LockingStart()
 {
+	ASSERT( !SchedulerIsCritical(),
+			LOCKING_START_NOT_CRIT,
+			"We can only start a locking session when not critical");
 	SchedulerStartCritical();
 }
 
 void LockingEnd(  )
 {
+	ASSERT( SchedulerIsCritical(),
+		 LOCKING_END_NOT_CRIT,
+		"If we are ending a locking session we must be critical already" );
 	SchedulerEndCritical();
 }
 
 void LockingSwitch( struct LOCKING_CONTEXT * context )
 {
+	ASSERT( SchedulerIsCritical(),
+			LOCKING_SWITCH_NOT_CRIT,
+			"must be crit to switch");
+
 	if( context == NULL )
 	{
+		//There is no context, so the thread must be on blocking path.
 		context = & SchedulerGetActiveThread()->LockingContext;
+		ASSERT( context->State != LOCKING_STATE_WAITING,
+			 LOCKING_SWITCH_NULL_WAITING,
+			"if the context is null, its implied the thread will block, not wait" );
 	}
-
+	else
+	{
+		//There is a context, so thread must be on waiting path.
+		ASSERT( context->State != LOCKING_STATE_BLOCKING,
+			 LOCKING_SWITCH_CONTEXT_BLOCKING,
+			"when a context is provided its assumed we will not block" );
+	}
+	
 	switch( context->State )
 	{
 		case LOCKING_STATE_BLOCKING:
@@ -33,13 +54,12 @@ void LockingSwitch( struct LOCKING_CONTEXT * context )
 
 		case LOCKING_STATE_CHECKED:
 		case LOCKING_STATE_ACQUIRED:
+			//we acquired the lock, continue
 			SchedulerEndCritical();
 			break;
-
-		default:
-			KernelPanic( LOCKING_SWITCH_INVAID_STATE );
-			SchedulerForceSwitch();
-			break;
+		case LOCKING_STATE_READY:
+			//lock is marked as ready, this is illegal
+			KernelPanic( LOCKING_SWITCH_CONTEXT_READY );
 	}
 }
 
@@ -50,13 +70,18 @@ void LockingInit( struct LOCKING_CONTEXT * context )
 
 void LockingAcquire( struct LOCKING_CONTEXT * context )
 {
+	ASSERT( SchedulerIsCritical(),
+			LOCKING_ACQUIRE_NOT_CRIT,
+			"must be crit to acquire");
+
 	if( context == NULL )
 	{
 		//We must retrieve the context from the active thread.
 		context = & SchedulerGetActiveThread()->LockingContext;
 
 		//If the context is null, then we:
-		//locked successfully without blocking : state == ready or checked -> state == checked
+		//locked successfully without blocking : state == ready or 
+		//checked -> state == checked
 		if( context->State == LOCKING_STATE_READY || 
 				context->State == LOCKING_STATE_CHECKED )
 		{
@@ -71,9 +96,10 @@ void LockingAcquire( struct LOCKING_CONTEXT * context )
 	{
 		//if we were provided a context
 		//then we are in:
-		//locked successfully without waiting : state == ready or checked -> state == acquired
+		//locked successfully without waiting : state == ready or 
+		//checked -> state == acquired
 		if( context->State == LOCKING_STATE_READY ||
-			   	context->State == LOCKING_STATE_CHECKED )
+				context->State == LOCKING_STATE_CHECKED )
 		{
 			//we acquired lock right away, send notification
 			context->State = LOCKING_STATE_ACQUIRED;
@@ -102,15 +128,22 @@ void LockingAcquire( struct LOCKING_CONTEXT * context )
 
 union LINK * LockingBlock( union BLOCKING_CONTEXT * blockingInfo, struct LOCKING_CONTEXT * context )
 {
+	ASSERT( SchedulerIsCritical(),
+			LOCKING_BLOCK_IS_CRIT,
+			"Must be crit to block");
+
 	if( context == NULL )
 	{
 		//context is active thread, so block him.
 		context = & SchedulerGetActiveThread()->LockingContext;
 		context->State = LOCKING_STATE_BLOCKING;
-		struct THREAD * thread = BASE_OBJECT( context, struct THREAD, LockingContext );
+		struct THREAD * thread = BASE_OBJECT( 
+				context, 
+				struct THREAD, 
+				LockingContext );
 		ASSERT( thread == SchedulerGetActiveThread(),
 				LOCKING_BLOCK_WRONG_CONTEXT,
-				"the context must be owned by active thread");
+				"the context must be owned by active thread" );
 		SchedulerBlockThread( );
 	}
 	else
@@ -127,19 +160,32 @@ union LINK * LockingBlock( union BLOCKING_CONTEXT * blockingInfo, struct LOCKING
 
 BOOL LockingIsAcquired( struct LOCKING_CONTEXT * context )
 {
-	if( context->State == LOCKING_STATE_ACQUIRED )
+	SchedulerStartCritical();
+	ASSERT( context != NULL, 
+			LOCKING_IS_ACQUIRED_CONTEXT_NULL,
+		   	"context must not be null" );
+
+	switch( context->State )
 	{
-		context->State = LOCKING_STATE_CHECKED;
-		return TRUE;
+		case LOCKING_STATE_ACQUIRED:
+			context->State = LOCKING_STATE_CHECKED;
+			SchedulerEndCritical();
+			return TRUE;
+			break;
+
+		case LOCKING_STATE_WAITING:
+			SchedulerEndCritical();
+			return FALSE;
+			break;
+
+		case LOCKING_STATE_READY:
+		case LOCKING_STATE_BLOCKING:
+		case LOCKING_STATE_CHECKED:
+			KernelPanic( LOCKING_IS_ACQUIRED_CONTEXT_IN_WRONG_STATE );
+			SchedulerEndCritical();
+			return FALSE;
+			break;
 	}
-	else if( context->State == LOCKING_STATE_WAITING )
-	{
-		return FALSE;
-	}
-	else
-	{
-		KernelPanic( LOCKING_IS_ACQUIRED_CONTEXT_IN_WRONG_STATE );
-		return FALSE;
-	}
+	return FALSE;
 }
 
