c17dcf213991a606fc6d777cdd367ed0cffc9a58
Andrew Thomson <andrewguy9@andrewguy9-laptop.(none)>
2008-04-02 10:59:27 -0700

    added target to build rules for kernel.

--- kernel/rules.mk
+++ kernel/rules.mk
@@ -1,33 +1,33 @@
 ######################
 #kernel
-kernel/startup.o: kernel/startup.c kernel/startup.h
+kernel/$(TARGET)_startup.o: kernel/startup.c kernel/startup.h
 
-kernel/timer.o: kernel/timer.c kernel/timer.h
+kernel/$(TARGET)_timer.o: kernel/timer.c kernel/timer.h
 
-kernel/hal.o: kernel/hal.c kernel/hal.h
+kernel/$(TARGET)_hal.o: kernel/hal.c kernel/hal.h
 
-kernel/scheduler.o: kernel/scheduler.c kernel/scheduler.h
+kernel/$(TARGET)_scheduler.o: kernel/scheduler.c kernel/scheduler.h
 
-kernel/mutex.o: kernel/mutex.c kernel/mutex.h
+kernel/$(TARGET)_mutex.o: kernel/mutex.c kernel/mutex.h
 
-kernel/semaphore.o: kernel/semaphore.c kernel/semaphore.h
+kernel/$(TARGET)_semaphore.o: kernel/semaphore.c kernel/semaphore.h
 
-kernel/resource.o: kernel/resource.c kernel/resource.h
+kernel/$(TARGET)_resource.o: kernel/resource.c kernel/resource.h
 
-kernel/panic.o: kernel/panic.c kernel/panic.h
+kernel/$(TARGET)_panic.o: kernel/panic.c kernel/panic.h
 
-kernel/sleep.o: kernel/sleep.c kernel/sleep.h
+kernel/$(TARGET)_sleep.o: kernel/sleep.c kernel/sleep.h
 
-kernel/interrupt.o: kernel/interrupt.c kernel/interrupt.h
+kernel/$(TARGET)_interrupt.o: kernel/interrupt.c kernel/interrupt.h
 
-kernel/socket.o: kernel/socket.c kernel/socket.h
+kernel/$(TARGET)_socket.o: kernel/socket.c kernel/socket.h
 
-kernel/io.o: kernel/io.c kernel/io.h
+kernel/$(TARGET)_io.o: kernel/io.c kernel/io.h
 
-kernel/pipe.o: kernel/pipe.c kernel/pipe.h
+kernel/$(TARGET)_pipe.o: kernel/pipe.c kernel/pipe.h
 
-kernel/locking.o: kernel/locking.c kernel/locking.h
+kernel/$(TARGET)_locking.o: kernel/locking.c kernel/locking.h
 
-kernel/worker.o: kernel/worker.c kernel/worker.h
+kernel/$(TARGET)_worker.o: kernel/worker.c kernel/worker.h
 
-kernel/gather.o: kernel/gather.c kernel/gather.h
+kernel/$(TARGET)_gather.o: kernel/gather.c kernel/gather.h
--- test/rules.mk
+++ test/rules.mk
@@ -1,15 +1,15 @@
-test/test0.o: test/test0.c
+test/$(TARGET)_test0.o: test/test0.c
 
-test/test1.o: test/test1.c
+test/$(TARGET)_test1.o: test/test1.c
 
-test/test2.o: test/test2.c
+test/$(TARGET)_test2.o: test/test2.c
 
-test/test3.o: test/test3.c
+test/$(TARGET)_test3.o: test/test3.c
 
-test/test4.o: test/test4.c
+test/$(TARGET)_test4.o: test/test4.c
 
-test/test5.o: test/test5.c
+test/$(TARGET)_test5.o: test/test5.c
 
-test/test6.o: test/test6.c
+test/$(TARGET)_test6.o: test/test6.c
 
-test/test7.o: test/test7.c
+test/$(TARGET)_test7.o: test/test7.c
