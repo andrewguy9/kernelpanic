08ebf5e471b5e0e4db0c6e2660b14294870be1a2
Andrew Thomson <andrewguy9@athomsonmac.local>
2008-12-20 15:26:01 -0800

    Update to makefile so that it determines whcih make program is running.
    This allows for BSD to build panic.

--- build_rules.mk
+++ build_rules.mk
@@ -17,10 +17,18 @@ KERN_PREFIX = kern_
 #Compiler Parameters
 ##############################################
 #c flags
-PC_CFLAGS = "-p -g -Wall"
 ifeq ($(shell uname), Darwin)
+PC_CFLAGS  = "-p -g -Wall"
 AVR_CFLAGS = "-Wall -mmcu=atmega128 -O2"
-else
+endif
+
+ifeq ($(shell uname), FreeBSD)
+PC_CFLAGS  = "-p -g -Wall"
+AVR_CFLAGS = "-Wall -mmcu=atmega128 -02"
+endif
+
+ifeq ($(shell uname), Linux)
+PC_CFLAGS  = "-p -g -Wall"
 AVR_CFLAGS = "-Wall -mmcu=atmega128 -gdwarf-2"
 endif
 ##############################################
@@ -46,7 +54,7 @@ DEBUG_DBG = DEBUG
 #BUILD_MACRO
 BUILD_APP = TEST_BUILD
 BUILD_KERNEL = KERNEL_BUILD
-
+MAKE_PROG = $_
 
 #############################################
 #combined strings
