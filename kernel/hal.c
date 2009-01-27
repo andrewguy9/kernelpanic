ac5eca40814c9007efa2d31956eeb11063f84857
Andrew Thomson <andrewguy9@athomsonmac.local>
2009-01-27 12:16:24 -0800

    Created separate linux/bsd and darwin hals.

--- kernel/hal.c
+++ kernel/hal.c
@@ -297,6 +297,8 @@ void HalSleepProcessor( )
 
 #ifdef PC_BUILD
 
+#ifdef DARWIN 
+
 #include<sys/time.h>
 #include<string.h>
 #include<signal.h>
@@ -625,5 +627,281 @@ void HalSleepProcessor()
 	ASSERT( !HalIsAtomic() );
 	pause();
 }
+
+#endif 
+
+#ifdef LINUX | BSD
+
+#include<sys/time.h>
+#include<string.h>
+#include<signal.h>
+#include<unistd.h>
+#include<stdio.h>
+
+#define AlarmSignal SIGALRM
+
+#define SAVE_STATE( context ) \
+	printf("getcontext(" #context ")\n"); \
+	(void)getcontext( &(context)->uc)
+
+#define RESTORE_STATE( context ) \
+	printf("setcontext(" #context ")\n"); \
+	(void)setcontext(&(context)->uc)
+
+#define SWITCH_CONTEXT( old, new ) \
+	printf("swapcontext(" #old "," #new ")\n"); \
+	(void)swapcontext(&((old)->uc), &((new)->uc))
+
+#define SET_SIGNAL(signum, handler) \
+	printf("signal(" #signum "," #handler ")\n"); \
+	signal(signum, handler )
+
+#define SIG_PROC_MASK( new, old ) \
+	printf("sigprocmask(" #new "," #old ")\n");  \
+	sigprocmask( SIG_SETMASK, new, old )
+
+char HAL_WATCHDOG_MASK;
+BOOL HalWatchdogOn;
+unsigned int HalWatchDogFrequency;
+unsigned int HalWatchdogCount;
+
+struct itimerval TimerInterval;
+
+//Used for reference.
+sigset_t EmptySet;
+sigset_t TimerSet;
+
+//Storage for the signal mask.
+sigset_t CurrentSet;
+
+//Timer Action Storage.
+struct sigaction TimerAction;
+
+//Prototype for later use.
+void HalLinuxTimer( int SignalNumber );
+
+void HalStartup()
+{
+	int status;
+
+	//Create the empty set.
+	status = sigemptyset( &EmptySet );
+	ASSERT( status == 0 );
+
+	//Create the timer set.
+	status = sigemptyset( &TimerSet );
+	ASSERT( status == 0 );
+	status = sigaddset( &TimerSet, AlarmSignal );
+	ASSERT( status == 0 );
+	ASSERT( sigismember( &TimerSet, AlarmSignal ) == 1 );
+
+	//Create the timer action.
+	TimerAction.sa_handler = HalLinuxTimer;
+	status = sigemptyset( &TimerAction.sa_mask);
+	ASSERT( status ==  0 );
+	TimerAction.sa_flags = 0;
+
+	//The current set should be equal to the timer set.
+	CurrentSet = TimerSet;
+	status = sigprocmask( SIG_SETMASK, &CurrentSet, NULL );
+	ASSERT( status == 0 );
+
+	ASSERT( HalIsAtomic() );
+
+	//Set up the watchdog.
+	HalWatchdogOn = FALSE;
+	HAL_WATCHDOG_MASK = 0;
+	HalWatchDogFrequency = 0;
+	HalWatchdogCount = 0;
+}
+
+void HalInitClock()
+{
+	int status;
+
+	//Turn on the timer signal handler.
+	status = sigaction( AlarmSignal, &TimerAction, NULL );
+	ASSERT( status == 0 );	
+
+	//Set the timer interval.
+	TimerInterval.it_interval.tv_sec = 0;
+	TimerInterval.it_interval.tv_usec = 1000;
+	TimerInterval.it_value.tv_sec = 0;
+	TimerInterval.it_value.tv_usec = 1000;
+	status = setitimer( ITIMER_REAL, &TimerInterval, NULL );
+	ASSERT(status == 0 );
+}
+
+void HalCreateStackFrame( 
+		struct MACHINE_CONTEXT * Context, 
+		void * stack, 
+		STACK_INIT_ROUTINE foo, 
+		COUNT stackSize)
+{
+
+
+	/* adjust to new context */
+	Context->uc.uc_link = NULL;
+	Context->uc.uc_stack.ss_sp = stack;
+	Context->uc.uc_stack.ss_size = stackSize;
+	Context->uc.uc_stack.ss_flags = 0;
+
+	/*make new context */
+	makecontext( &(Context->uc), foo, 0 );
+
+#ifdef DEBUG
+	//Set up the stack boundry.
+	Context->High = stack + stackSize;
+	Context->Low = stack;
+#endif
+}
+
+void HalGetInitialStackFrame( struct MACHINE_CONTEXT * Context )
+{
+	printf("hal creating idle loop frame\n");
+	SAVE_STATE(Context);
+
+#ifdef DEBUG
+	//The stack bounderies are infinite for the initial stack.
+	Context->High = (char *) -1;
+	Context->Low = (char *) 0;
+#endif
+}
+
+void HalContextSwitch( )
+{
+	struct MACHINE_CONTEXT * oldContext = ActiveStack;
+	struct MACHINE_CONTEXT * newContext = NextStack;
+
+	ActiveStack = NextStack;
+	NextStack = NULL;
+
+	SWITCH_CONTEXT(oldContext, newContext);
+}
+
+void HalSerialStartup()
+{
+	//TODO
+}
+
+BOOL HalIsAtomic()
+{
+	sigset_t curSet;
+	int status;
+
+	status = sigprocmask( 0, NULL, &curSet );
+	ASSERT( status == 0 );
+
+	status = sigismember( &curSet, AlarmSignal );
+	ASSERT( status != -1 );
+	if( status == 1 )
+	{
+		//Alarm was a member of the "blocked" mask, so we are atomic.
+		return TRUE;
+	}
+	else 
+	{
+		//Alarm was not a member of the "blocked" mask, so we are not atomic.
+		return FALSE; 
+	}
+}
+
+void HalDisableInterrupts()
+{
+	int status;
+
+	status = sigprocmask( SIG_SETMASK, &TimerSet, NULL ); 
+	ASSERT( status == 0 );
+
+	//We just disabled,
+	//update current set.
+	CurrentSet = TimerSet;
+
+	ASSERT( HalIsAtomic() );
+}
+
+void HalEnableInterrupts()
+{
+	int status;
+
+	//We are about to enable, update current set.
+	CurrentSet = EmptySet;
+
+	status = sigprocmask( SIG_SETMASK, &EmptySet, NULL );
+	ASSERT( status == 0 );
+
+	ASSERT( ! HalIsAtomic() );
+}
+
+//prototype for handler.
+void TimerInterrupt();
+
+/*
+ * Acts like the hardware clock.
+ * Calls TimerInterrupt if he can.
+ */
+void HalLinuxTimer( int SignalNumber )
+{
+	int status;
+
+	//The kernel should add this signal to the blocked list inorder to avoid 
+	//nesting calls the the handler.
+	//verify this.
+	ASSERT( HalIsAtomic() );
+
+	//The current mask changed when this was called. 
+	//save the change over the mask.
+	CurrentSet = TimerSet;
+
+	//TODO IF POSSIBLE, MOVE WATCHDOG INTO OWN TIMER.
+	//Run the watchdog check
+	if( HalWatchdogOn )
+	{
+		HalWatchdogCount ++;
+
+		if( HalWatchdogCount >= HalWatchDogFrequency )
+		{
+			//The time for a match has expried. Panic!!!
+			HalPanic("Watchdog Timeout", 0 );
+		}
+	}
+
+	//Call the kernel's timer handler.
+	TimerInterrupt();
+
+	//There is an implied enable interrupts call when the timer returns.
+}
+
+void HalResetClock()
+{
+	//TODO NO NOTHING FOR NOW SINCE ALARM IS OUR GLOBAL FLAG.
+	//and the alarm is already periodic. (unlike the avr).
+}
+
+void HalPanic(char file[], int line)
+{
+	printf("PANIC: %s:%d\n",file,line);
+	exit(-1);
+}
+
+void HalEnableWatchdog( int frequency )
+{
+	HalWatchdogOn = TRUE;
+	HalWatchDogFrequency = frequency;
+}
+
+void HalPetWatchdog( )
+{
+	HalWatchdogCount = 0;	
+}
+
+void HalSleepProcessor()
+{
+	ASSERT( !HalIsAtomic() );
+	pause();
+}
+
+#endif
 #endif //end of pc build
 //-----------------------------------------------------------------------------
+
--- kernel/hal.h
+++ kernel/hal.h
@@ -91,7 +91,10 @@ void HAL_NAKED_FUNCTION HalContextSwitch();
 //
 #ifdef PC_BUILD
 
-#include<setjmp.h>
+#ifdef LINUX | BSD
+
+#include<ucontext.h>
+#include<stdio.h>
 #include<stdlib.h>
 
 /*
@@ -102,6 +105,36 @@ void HAL_NAKED_FUNCTION HalContextSwitch();
  *
  * We can use system V system calls to switch to and from our
  * ucontext_t.
+ */
+
+struct MACHINE_CONTEXT
+{
+	INDEX Flag;//Thread number which gets used for the watchdog and debug leds.
+	STACK_INIT_ROUTINE Foo;//Pointer to the first function the thread calls.
+	ucontext_t uc;
+
+#ifdef DEBUG
+	//Counters to keep track of thread usage.
+	COUNT TimesRun;
+	COUNT TimesSwitched;
+	//Pointers to the top and bottom of the stack. Used to detect stack overflow.
+	char * High;
+	char * Low;
+#endif
+};
+
+#endif
+
+#ifdef DARWIN
+
+#include<setjmp.h>
+#include<stdlib.h>
+
+/*
+ * On Darwin machines we maintain our machine context using a
+ * jmp_buf array. This sturucture stores raw register state.
+ *
+ * We can use setjmp and longjmp to save and restore the register state.
  */
 
 struct MACHINE_CONTEXT
@@ -119,6 +152,9 @@ struct MACHINE_CONTEXT
 	char * Low;
 #endif
 };
+
+#endif
+
 
 BOOL HalIsAtomic();
 void HalDisableInterrupts();
