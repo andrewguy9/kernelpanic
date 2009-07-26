c9042398ef00a30543674a18402895f41c0eb8b2
Andrew Thomson <andrewguy9@athomsonmac-4.local>
2009-07-26 00:05:24 -0700

    Commenting out prints in regression.pl.

--- regression.pl
+++ regression.pl
@@ -17,17 +17,17 @@ while( $test = <REGRESSION_TESTS> ) {
 
 	#start the current test.
 	chomp( $test );
-	print "Starting $test\n";
+	#print "Starting $test\n";
 	$pid = fork();
 
 	if(not defined $pid) {
 		print "failed to fork\n";
 	} elsif ($pid == 0) {
-		print "i'm a child, running $test\n";
+		#print "i'm a child, running $test\n";
 		exec "./$test";
 		die "failed to start $test\n";
 	} else {
-		print "i'm the parent, adding $pid to list\n";
+		#print "i'm the parent, adding $pid to list\n";
 		$pids{ $pid } = $test;
 		$children = $children + 1;
 	}
