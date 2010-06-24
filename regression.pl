d0124bece119d483043f24517964b42bf10b80e6
Andrew Thomson <andrewguy9@athomsonmac.local>
2010-06-24 14:34:58 -0700

    Overhauled the regression.pl script. Still OSX only, but now runs tests one at a time.
    Should cause fewer watchdog false failures.

--- regression.pl
+++ regression.pl
@@ -1,24 +1,36 @@
 #!/usr/bin/perl
 
+use warnings;
+use strict;
+
+#Set STDOUT to flush right away
+local $| = 1;
+
+my @failures = ();
+
 use File::Copy;
 
-%pids = {};
-$children = 0;
+#Get timeout for tests.
+my $timeout = $ARGV[0];
+if(!$timeout) {
+	print "Assuming timeout of 60\n";
+	$timeout = 60;
+}
 
-#install signal handlers 
-
-
-#run regression tests.
+#Get regression tests to run.
 open(REGRESSION_TESTS, "regression.txt");
 
+#Make sure cores are generated.
 system "ulimit -c unlimited";
 
-while( $test = <REGRESSION_TESTS> ) {
+while( my $test = <REGRESSION_TESTS> ) {
 
 	#start the current test.
 	chomp( $test );
+
+	my $killed = 0;
 	#print "Starting $test\n";
-	$pid = fork();
+	my $pid = fork();
 
 	if(not defined $pid) {
 		print "failed to fork\n";
@@ -28,23 +40,46 @@ while( $test = <REGRESSION_TESTS> ) {
 		die "failed to start $test\n";
 	} else {
 		#print "i'm the parent, adding $pid to list\n";
-		$pids{ $pid } = $test;
-		$children = $children + 1;
+		print "$test running as pid $pid...";
+
+		#set up a timer to break us out after some time.
+		local $SIG{ALRM} = sub{
+			$killed = 1;
+			kill 'INT', $pid;
+			#die 'alarm';#dont know why he wants to die!
+		};
+		alarm $timeout;
+
+		my $kid = wait;
+		local $SIG{ALRM} = undef;
+		if ($kid == -1) {
+			die "wait returned -1"
+		} else {
+			if($killed) {
+				print " passed!\n";
+			} else {
+				print " ***died unexpectedly*** ";
+				my $core = "./$test.$kid.core";
+				push @failures, "gdb $test $core";
+				move("/cores/core.$kid", "$core");
+				print " Core left at $core ";
+				print "\n";
+			}
+		}
 	}
 }
 
-print "all processes running\n";
+print "all done testing\n";
 
-while( $children > 0 ) {
-	$dead = wait;
-	if ($dead == -1) {
-		die "wait returned -1"
-	} else {
-		print "pid $dead  died, which is test $pids{$dead} is dead\n";
-		$children = $children -1;
-		print "moving /cores/core.$dead to ./$pids{$dead}.core\n";
-		move("/cores/core.$dead", "./$pids{$dead}.core");
+print "\n\n";
+print "Summary:\n";
+print "-------------------------------------------------------------------------------\n";
+if(@failures) {
+	for my $cur (@failures) {
+		print "$cur\n";
 	}
+} else {
+	print "All tests passed!\n";
 }
-
-print "all done\n";
+print "-------------------------------------------------------------------------------\n";
+	print "done\n";
