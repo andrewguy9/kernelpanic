81c222f898627e965f9c77378d469460764e8bee
Andrew Thomson <andrewguy9@athomsonmac.local>
2010-08-06 20:41:33 -0700

    Removing the metrics unit, and associated tests.

--- utils/rules.mk
+++ utils/rules.mk
@@ -12,8 +12,6 @@ utils/$(TARGET)_bham.o: utils/bham.c utils/bham.h
 
 utils/$(TARGET)_avl.o: utils/avl.c utils/avl.h
 
-utils/$(TARGET)_metrics.o: utils/metrics.c utils/metrics.h
-
 ###################
 #tests
 utils/$(TARGET)_linktest.o: utils/linktest.c 
@@ -25,5 +23,3 @@ utils/$(TARGET)_ringtest.o: utils/ringtest.c
 utils/$(TARGET)_bitmaptest.o: utils/bitmaptest.c
 
 utils/$(TARGET)_bhamtest.o: utils/bhamtest.c
-
-utils/$(TARGET)_metricstest.o: utils/metricstest.c
