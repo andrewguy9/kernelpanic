0b9d9ae9e4478866bd62e10f2d5fa3d71cdb7408
Andrew Thomson <andrewguy9@athomsonmac.local>
2011-04-28 18:26:43 -0700

    Added blocking to the SerialRead function. Added a test for serial.

--- kernel/serial.c
+++ kernel/serial.c
@@ -2,6 +2,7 @@
 #include"hal.h"
 #include"critinterrupt.h"
 #include"../utils/ringbuffer.h"
+#include"signal.h"
 
 #include<stdio.h>
 
@@ -12,6 +13,10 @@ char SerialOutputBuffer[BUFFER_SIZE];
 
 struct RING_BUFFER SerialInputRing;
 struct RING_BUFFER SerialOutputRing;
+
+struct HANDLER_OBJECT GetBytesCritObject;
+
+struct SIGNAL GetBytesSignal;
 
 void SendBytesInterrupt(void)
 {
@@ -27,13 +32,11 @@ void SendBytesInterrupt(void)
 	IsrDecrement(IRQ_LEVEL_SERIAL_WRITE);
 }
 
-void GetBytesInterrupt(void)
+BOOL GetBytesCritHandler(struct HANDLER_OBJECT * handler)
 {
-	IsrIncrement(IRQ_LEVEL_SERIAL_READ);
+	IsrDisable(IRQ_LEVEL_SERIAL_WRITE);
 
-	ASSERT(IsrIsAtomic(IRQ_LEVEL_SERIAL_READ));
 	while(!RingBufferIsFull(&SerialInputRing)) {
-
 		char data;
 		if(HalSerialGetChar(&data)) {
 			ASSUME(RingBufferWrite(&data, sizeof(data), &SerialInputRing), 1);
@@ -42,6 +45,23 @@ void GetBytesInterrupt(void)
 		}
 	}
 
+	IsrEnable(IRQ_LEVEL_SERIAL_WRITE);
+	
+	// Here there is a race between us adding the new bytes and someone 
+	// taking them out before we signal. This can lead to spurious signaling.
+	// On this side we don't care if we signal too often because at worst we 
+	// trick someone into reading a zero byte read.
+	SignalSet( &GetBytesSignal );
+	return TRUE;
+}
+
+void GetBytesInterrupt(void)
+{
+	IsrIncrement(IRQ_LEVEL_SERIAL_READ);
+
+	if( HandlerIsFinished(&GetBytesCritObject) )
+		CritInterruptRegisterHandler( &GetBytesCritObject, GetBytesCritHandler, NULL );
+
 	IsrDecrement(IRQ_LEVEL_SERIAL_READ);
 }
 
@@ -49,7 +69,11 @@ void SerialStartup()
 {
 	RingBufferInit( SerialInputBuffer, BUFFER_SIZE, &SerialInputRing );
 	RingBufferInit( SerialOutputBuffer, BUFFER_SIZE, &SerialOutputRing );
+
+	HandlerInit( &GetBytesCritObject );
 	
+	SignalInit( &GetBytesSignal, FALSE );
+
 	HalRegisterIsrHandler( SendBytesInterrupt, (void *) HAL_ISR_SERIAL_WRITE, IRQ_LEVEL_SERIAL_WRITE);
 	HalRegisterIsrHandler( GetBytesInterrupt, (void *) HAL_ISR_SERIAL_READ, IRQ_LEVEL_SERIAL_READ);
 
@@ -71,12 +95,45 @@ COUNT SerialWrite(char * buf, COUNT len)
 
 COUNT SerialRead(char * buf, COUNT len)
 {
-	COUNT read;
+	COUNT read = 0;
+	BOOL wasFull = FALSE;
 
-	IsrDisable(IRQ_LEVEL_SERIAL_READ);
-	read = RingBufferRead(buf, len, &SerialInputRing);
-	IsrEnable(IRQ_LEVEL_SERIAL_READ);
+	do {
+		BOOL empty = FALSE;
 
+		IsrDisable(IRQ_LEVEL_SERIAL_READ);
+
+		if( RingBufferIsFull( &SerialInputRing ) ) {
+			wasFull = TRUE;
+		}
+
+		read = RingBufferRead(buf, len, &SerialInputRing);
+		if( RingBufferIsEmpty( &SerialInputRing ) ) {
+			empty = TRUE;
+		}
+
+		IsrEnable(IRQ_LEVEL_SERIAL_READ);
+
+		// Here there is a race between us adding the new bytes and someone 
+		// taking them out before we signal. This can lead to spurious signaling.
+		// We care about spurious signaling on this side because we can accidentally cause
+		// all readers to block when there is data available. This is why we always attempt
+		// a read before blocking on zero data. THIS DOES NOT FIX THE RACE, it just makes
+		// it less of an issue when it happens.
+		if( empty ) {
+			SignalUnset( &GetBytesSignal );
+		}
+
+		if(read == 0) {
+			SignalWaitForSignal( &GetBytesSignal, NULL );
+		}
+	} while(read == 0);
+
+	// We were full, so lets make sure there wasn't any data buffered
+	// in the hal.
+	if( wasFull ) {
+		HalRaiseInterrupt( IRQ_LEVEL_SERIAL_READ );
+	}
 	return read;
 }
 
--- test/test10.c
+++ test/test10.c
@@ -4,10 +4,7 @@
 #include"../kernel/serial.h"
 
 /*
- * Starts three threads each with different quantums.
- * Each thread increments a variable, Value1 2 and 3.
- * Value1 = 2 * Value2 
- * Value2 = 2 * Value3.
+ * Reads from STDIN and prints the results to STDOUT. 
  */
 
 //
@@ -16,18 +13,13 @@
 
 #define BUF_SIZE 512
 
-void Test1ThreadMain( void * arg )
+void TestThreadMain( void * arg )
 {
-	COUNT * var = (COUNT *) arg;
 	char buf[BUF_SIZE];
 	COUNT read;
 
-	volatile unsigned char a = 0;
 	while( 1 )
 	{
-		(*var)++;
-		for(a=1;a>0;a++);
-
 		read = SerialRead(buf, BUF_SIZE);
 		buf[read]= '\0';
 		if(read > 0) {
@@ -38,13 +30,6 @@ void Test1ThreadMain( void * arg )
 	}
 }
 
-//
-//Vars 
-//
-
-COUNT Value1 = 0;
-COUNT Value2 = 0;
-COUNT Value3 = 0;
 
 //
 //Thread structures
@@ -52,14 +37,8 @@ COUNT Value3 = 0;
 
 #define STACK_SIZE HAL_MIN_STACK_SIZE
 
-struct THREAD TestThread1;
-char TestThread1Stack[STACK_SIZE];
-
-struct THREAD TestThread2;
-char TestThread2Stack[STACK_SIZE];
-
-struct THREAD TestThread3;
-char TestThread3Stack[STACK_SIZE];
+struct THREAD TestThread;
+char TestThreadStack[STACK_SIZE];
 
 //
 //Main
@@ -75,33 +54,13 @@ int main()
 	
 	//Initialize Threads
 	SchedulerCreateThread(
-			&TestThread1,
+			&TestThread,
 			2,
-			TestThread1Stack,
+			TestThreadStack,
 			STACK_SIZE,
-			Test1ThreadMain,
-			&Value1,
+			TestThreadMain,
+			NULL,
 		   	4,
-			TRUE);
-
-	SchedulerCreateThread(
-			&TestThread2,
-			4,
-			TestThread2Stack,
-			STACK_SIZE,
-			Test1ThreadMain,
-			&Value2,
-			5,
-			TRUE);
-
-	SchedulerCreateThread(
-			&TestThread3,
-			8,
-			TestThread3Stack,
-			STACK_SIZE,
-			Test1ThreadMain,
-			&Value3,
-			6,
 			TRUE);
 
 	KernelStart();
