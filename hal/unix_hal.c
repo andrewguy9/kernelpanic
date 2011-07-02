79f46fb786abb4787092bd5249aed9ed98bacbd4
Andrew Thomson <andrewguy9@athomsonmac.gateway.2wire.net>
2011-07-02 14:34:38 -0700

    Adding ATOMIC type.
    Today we achieve all syncronization by messing 
    with IRQ levels. This is an inefficient way of 
    providing mutual exclusion. 
    
    This change introduces an ATOMIC type which can be
    used with the HalAtomic* functions to use memory
    for synchronization.

--- hal/unix_hal.c
+++ hal/unix_hal.c
@@ -536,10 +536,17 @@ void HalSerialStartup()
 	//TODO Fill in with rest of Serial IO Unit.
 }
 
+//
+//Atomic Management
+//
 
+ATOMIC HalAtomicGetAndOr(ATOMIC * var, ATOMIC val)
+{
+	return __sync_fetch_and_or(var, val);
+}
 
-
-
-
-
+ATOMIC HalAtomicGetAndAnd(ATOMIC * var, ATOMIC val)
+{
+	return __sync_fetch_and_and(var, val);
+}
 
--- utils/types.h
+++ utils/types.h
@@ -10,7 +10,7 @@ typedef unsigned int COUNT;
 typedef unsigned int INDEX;
 typedef unsigned char BOOL;
 typedef unsigned long int TIME;
-
+typedef unsigned char ATOMIC;
 #endif
 
 #ifdef PC_BUILD
@@ -18,7 +18,7 @@ typedef unsigned long int COUNT;
 typedef unsigned long int INDEX;
 typedef unsigned char BOOL;
 typedef unsigned long int TIME;
-
+typedef unsigned int ATOMIC;
 #endif
 
 #endif // TYPES_H
