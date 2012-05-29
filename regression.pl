06d1b7155ccdbd721ce71913ddc5133a7760fb2c
Andrew Thomson <athomsonguy@gmail.com>
2012-05-28 23:25:09 -0700

    Overhauled regression.pl
    Created new process which wraps each test.
    Moved timing code into new wrapper process.
    Removed batching code, replaced with running process count.

--- regression.pl
+++ regression.pl
@@ -25,14 +25,11 @@ print "Runs $runs\n";
 #Set STDOUT to flush right away
 local $| = 1;
 
-my %cores = (); # Key is test, value is pid.
-
 my @tests = ();
 if (@ARGV) {
         push @tests, @ARGV;
 } else {
         print "Sourcing tests from regression.txt\n";
-        print "ARGV was @ARGV\n";
         open(REGRESSION_TESTS, "regression.txt");
 
         while( my $test = <REGRESSION_TESTS> ) {
@@ -41,91 +38,32 @@ if (@ARGV) {
         }
 }
 
+my @tmp_tests = ();
 for (my $i=0; $i<$runs; $i++) {
-        push @tests, @tests;
+        push @tmp_tests, @tests;
 }
+@tests = @tmp_tests;
 
 # Run tests
-print "Setting up batch - - - - - - - - - - - - - - - - - - - - - - - - \n" if $batchsize != 1;
-my %kids = ();
-my $should_wait = min($batchsize, scalar @tests);
-my $passed = 0;
-my @successes = ();
-my @failures = ();
-while (@tests) {
-        my $test = pop @tests;
+my $running = 0;
+while (@tests || $running > 0)
+{
+        if ($running < $batchsize && @tests)
+        {
+                my $test = pop @tests;
 
-        my $pid = fork();
-        if(not defined $pid) {
-                die "failed to fork\n";
-        } elsif ($pid == 0) {
-                exec "./$test";
-                die "failed to start $test\n";
-        }
-
-        #print "i'm the parent, adding $pid to list\n";
-        $kids{ $pid } = $test;
-
-        $should_wait--;
-        #We have a full batch.
-        if( $should_wait == 0 ) {
-                #set up a timer to break us out after some time.
-                local $SIG{ALRM} = sub{
-                        $passed = 1;
-                        for my $kid (keys %kids) {
-                                kill 'INT', $kid;
-                        }
-                };
-                alarm $timeout;
-
-                #wait for all kids to finish.
-                while( %kids ) {
-                        my $dead = wait;
-                        if ($dead == -1) {
-                                die "wait returned -1"
-                        }
-                        if($passed) {
-                                push @successes, "Test $kids{$dead}($dead)... Passed";
-                        } else {
-                                push @failures, "Test $kids{$dead}($dead)... FAILED!!!";
-                                $cores{$kids{$dead}} = $dead;
-                        }
-                        delete $kids{$dead};
-                } # While kids still around
-                my @summary = (@successes, @failures);
-                @summary = sort @summary;
-                for my $line (@summary) {
-                        print "$line\n";
+                my $pid = fork();
+                if ($pid == 0) {
+                        runtest($test);
+                } else {
+                        print "Starting $test on $pid\n";
+                        $running++;
                 }
-                print "Setting up batch - - - - - - - - - - - - - - - - - - - - - - - - \n" if $batchsize != 1;
-                local $SIG{ALRM} = undef;
-                @successes = ();
-                @failures = ();
-                %kids = ();
-                $should_wait = min($batchsize, scalar @tests);
-                $passed = 0;
-        } # Should Wait
-}
-
-my @stacks = ();
-print "all done testing, gathering cores\n";
-for my $test (sort keys %cores) {
-        my $core = "./$test.$cores{$test}.core";
-        move("/cores/core.$cores{$test}", "$core");
-        print "Core left at $core\n";
-        push @stacks, get_stack($test, $core);
-}
-
-print "\n\n";
-print "Summary:\n";
-print "-"x80 . "\n";
-if(@stacks) {
-        for my $stack (@stacks) {
-                print $stack;
-                print "-"x80 . "\n";
+        } else {
+                my $pid = wait;
+                print "Reaped $pid with status $?\n";
+                $running--;
         }
-} else {
-        print "All tests passed!\n";
 }
 print "done\n";
 
@@ -135,7 +73,8 @@ sub get_stack {
 
         my $stack = "gdb $program $core\n";
 
-        open GDB_OUTPUT, "gdb --command ./get_stack.gdb $program $core |" or die "Could not execute: $!";
+        open GDB_OUTPUT, "gdb --command ./get_stack.gdb $program $core |"
+                or die "Could not execute: $!";
         while(<GDB_OUTPUT>) {
                 chomp $_;
                 if($_ =~ /^#\d*\s*0x([0-9]|[a-f])* in .* at .*:\d*$/) {
@@ -145,4 +84,38 @@ sub get_stack {
         close GDB_OUTPUT;
 
         return $stack;
+}
+
+my $test_name;
+my $test_passed;
+my $test_pid;
+sub runtest
+{
+
+        $test_name = shift @_;
+        $test_passed = 0;
+
+        #set up a timer to break us out after some time.
+        local $SIG{ALRM} = sub{
+                $test_passed = 1;
+                        kill 'INT', $test_pid;
+        };
+        alarm $timeout;
+
+        $test_pid = fork;
+        if($test_pid == 0) {
+                exec "./$test_name";
+                die "failed to start $test_name\n";
+        }
+
+        my $dead = wait;
+        if($test_passed) {
+                exit(0);
+        } else {
+                my $core = "./$test_name.$test_pid.core";
+                move("/cores/core.$test_pid", "$core");
+                print "Core left at $core\n";
+                print get_stack($test_name, $core);
+                exit(1);
+        }
 }
