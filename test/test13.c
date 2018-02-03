4dddd8e6ebebe5501f6234e13003e28740d538e7
Andrew Thomson <athomsonguy@gmail.com>
2018-02-03 11:08:15 -0800

    Add a panic runtime test.
    Test 13 runs threads seeking to find the first 1M primes. These threads run totally independently. 
    
    First tests show that adding a 2nd thread adds about 3-5% overhead from context switching.

---
+++ test/test13.c
@@ -0,0 +1,63 @@
+#include"utils/primes.h"
+#include"kernel/hal.h"
+#include"kernel/startup.h"
+#include"kernel/scheduler.h"
+#include"kernel/panic.h"
+#include"kernel/gather.h"
+
+#define PRIME_TAG1 "test13_primes1"
+#define PRIME_TAG2 "test13_primes2"
+
+#define STACK_SIZE HAL_MIN_STACK_SIZE
+
+char PrimesStack1[STACK_SIZE];
+struct THREAD PrimesThread1;
+
+char PrimesStack2[STACK_SIZE];
+struct THREAD PrimesThread2;
+
+struct GATHER PrimesGather;
+
+THREAD_MAIN PrimesMain;
+void PrimesMain(void * context) {
+  char * tag = context;
+  INDEX max = 1000000;
+  COUNT buffer_size = 100000;
+  int * primes_buffer = HalMap(tag, NULL, sizeof(int) * buffer_size);
+  enum PRIMES_STATUS status = findPrimes(max, primes_buffer, buffer_size);
+  if (status == PRIMES_OK) {
+    GatherSync( &PrimesGather, NULL);
+    GeneralPanic(); //TODO SOME KIND OF SIGNAL THAT WE SUCCEEDED.
+  } else {
+    KernelPanic();
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  KernelInit();
+  SchedulerStartup();
+  GatherInit( &PrimesGather, 2);
+
+  SchedulerCreateThread(
+      &PrimesThread1,
+      100,
+      PrimesStack1,
+      STACK_SIZE,
+      PrimesMain,
+      PRIME_TAG1,
+      TRUE);
+
+  SchedulerCreateThread(
+      &PrimesThread2,
+      100,
+      PrimesStack2,
+      STACK_SIZE,
+      PrimesMain,
+      PRIME_TAG1,
+      TRUE);
+
+  KernelStart();
+
+  return 0;
+}
