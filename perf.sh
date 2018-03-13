f72aa173d20f3b702ae05317d8a5adbf6f439ee4
Andrew Thomson <athomsonguy@gmail.com>
2018-03-13 14:52:08 -0700

    remove extra spaces in csv

--- perf.sh
+++ perf.sh
@@ -6,8 +6,8 @@ jobid=$1
 prog=$2
 
 #TODO we don't need the stat name.
-stats=$({ time $prog 2> /dev/null ; } 2>&1 | perl -ne '/^(\w+)\s+(\d+)m(\d+\.\d+)s$/ && print "$1, ". ($2*60 + $3) .","' | perl -pe 's/(.*),$/$1/')
+stats=$({ time $prog 2> /dev/null ; } 2>&1 | perl -ne '/^(\w+)\s+(\d+)m(\d+\.\d+)s$/ && print "$1,". ($2*60 + $3) .","' | perl -pe 's/(.*),$/$1/')
 #TODO We should not generate a report if the program crashed.
 dirty=$(git status --porcelain | perl -ne '/^ M (.*)/&& print "$1, "')
 
-echo $jobid, $branch, $dirty, $prog, $stats | tee -a ./perf.csv
+echo $jobid,$branch,$dirty,$prog,$stats | tee -a ./perf.csv
