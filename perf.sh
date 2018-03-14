1529fae92061df43c0041b14f12b7b328c01ef73
Andrew Thomson <athomsonguy@gmail.com>
2018-03-13 17:04:51 -0700

    Don’t log time data on program crash.

--- perf.sh
+++ perf.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 branch=$(git branch -v| awk '{if($1=="*"){print $2","$3}}')
 
 jobid=$1
 prog=$2
 
-stats=$({ time $prog 2> /dev/null ; } 2>&1 | perl -ne '/^(\w+)\s+(\d+)m(\d+\.\d+)s$/ && print "" . ($2*60 + $3) .","' | perl -pe 's/(.*),$/$1/')
-#TODO We should not generate a report if the program crashed.
+timestr=$({ time $prog 2> /dev/null ; } 2>&1)
+stats=$(echo "$timestr" | perl -ne '/^(\w+)\s+(\d+)m(\d+\.\d+)s$/ && print "" . ($2*60 + $3) .","' | perl -pe 's/(.*),$/$1/')
 dirty=$(git status --porcelain | perl -ne '/^ M (.*)/&& print "$1;"' | perl -pe 's/(.*);$/$1/')
 
 echo $jobid,$branch,$dirty,$prog,$stats | tee -a ./perf.csv
