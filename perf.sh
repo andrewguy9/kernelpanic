0980a230cb4a54f41cef8da7aa8e505d4c01dcbc
Andrew Thomson <athomsonguy@gmail.com>
2018-03-13 18:33:49 -0700

    apply shell feedback to perf.sh

--- perf.sh
+++ perf.sh
@@ -11,4 +11,4 @@ timestr=$({ time $prog 2> /dev/null ; } 2>&1)
 stats=$(echo "$timestr" | perl -ne '/^(\w+)\s+(\d+)m(\d+\.\d+)s$/ && print "" . ($2*60 + $3) .","' | perl -pe 's/(.*),$/$1/')
 dirty=$(git status --porcelain | perl -ne '/^ M (.*)/&& print "$1;"' | perl -pe 's/(.*);$/$1/')
 
-echo $jobid,$branch,$dirty,$prog,$stats | tee -a ./perf.csv
+echo "$jobid,$branch,$dirty,$prog,$stats" | tee -a ./perf.csv
