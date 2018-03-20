da6d6524ea0997984001511c8f08b9db1a7d58f8
Andrew Thomson <athomsonguy@gmail.com>
2018-03-20 11:58:48 -0700

    Stacks use STACK_INIT_ROUTINES, not THREAD_MAINs.

--- kernel/context.h
+++ kernel/context.h
@@ -9,7 +9,7 @@ typedef void THREAD_MAIN(void * arg ); //TODO CHANGE NAME.
 // AKA STACK
 struct CONTEXT {
   struct MACHINE_CONTEXT MachineState;
-  THREAD_MAIN * Main; // Not needed for ctx switch, but needed for bootstrapping.
+  STACK_INIT_ROUTINE  * Main; // Not needed for ctx switch, but needed for bootstrapping.
   void * MainArg; // Not needed for ctx switch, but needed for bootstrapping.
 
 #ifdef DEBUG
