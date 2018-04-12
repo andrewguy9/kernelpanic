76b2b3e5b473489384a94e4ab1071c1b29ae4ebe
Andrew Thomson <athomsonguy@gmail.com>
2018-04-11 17:05:55 -0700

    Remove functions from ring buffer.

--- utils/ringbuffer.h
+++ utils/ringbuffer.h
@@ -12,9 +12,7 @@ struct RING_BUFFER {
   BOOL Empty;
 };
 
-COUNT RingBufferRead( char * buff, COUNT size, struct RING_BUFFER * ring ); //TODO RETIRE.
 void RingBufferReadBuffer(SPACE * space, struct RING_BUFFER * ring);
-COUNT RingBufferWrite( char * buff, COUNT size, struct RING_BUFFER * ring ); //TODO RETIRE
 void RingBufferWriteBuffer(DATA * data, struct RING_BUFFER * ring);
 BOOL RingBufferIsEmpty( struct RING_BUFFER * ring );
 BOOL RingBufferIsFull( struct RING_BUFFER * ring );
