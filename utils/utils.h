409b8c4d4b77c754024fa1613798e236006772a9
Andrew Thomson <athomsonguy@gmail.com>
2018-04-25 18:27:04 -0700

    Make a simpler version of FOR_EACH.
    Infer type if item.
    Don’t next for loops.

--- utils/utils.h
+++ utils/utils.h
@@ -73,16 +73,19 @@
 #endif //DEBUG
 
 // https://stackoverflow.com/questions/400951/does-c-have-a-foreach-loop-construct
-#if 1
+#if 0
 #define FOR_EACH(item, array) \
   for(int keep = 1, \
       count = 0,\
       size = sizeof (array) / sizeof *(array); \
       keep && count != size; \
       keep = !keep, count++) \
-      for(item = (array) + count; keep; keep = !keep)
+      for(typeof(array[0]) * item = (array) + count; keep; keep = !keep)
 #else
-#define FOR_EACH(item, array) \
-  for (int count = 0, * item = (array) + count; count < sizeof(array) / sizeof *(array); ++count)
+#define FOR_EACH(item, array)                                            \
+  for(typeof(*(array)) *item = array,                                    \
+                       *stop = array + sizeof (array) / sizeof *(array); \
+      item != stop;                                                      \
+      item++)
 #endif
 #endif //UTILS_H
--- utils/utilstest.c
+++ utils/utilstest.c
@@ -1,10 +1,11 @@
 #include"utils.h"
 
 #include<stdio.h>
+
 void test_foreach() {
   int values[9] = {1,2,3,4,5,6,7,8,9};
   int sum = 0;
-  FOR_EACH (int * i, values) {
+  FOR_EACH (i, values) {
     printf("loop: %p = %i\n", i, *i);
     sum+=*i;
   }
