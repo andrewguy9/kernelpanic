3d10278573f25d0242c4905a3d7e73790a10986e
Andrew Thomson <athomsonguy@gmail.com>
2019-05-28 00:38:54 -0700

    example with blocks
    make types explicit.
    
    try out a macro forEach.
    
    Turn forEach into forEachDefine.
    
    Turn forEachDoer into macro.
    
    forEachDefine -> forEachType.
    
    make macros explicit for experiment.
    
    Made types anonymous.
    
    Remove doer Type.
    
    Use new macro.
    
    use in-place structure definition.
    
    restructure map.
    
    expand map inline.
    
    inline inc definition
    
    explicit filter.
    
    turn to blocks
    
    inline typedef.
    
    macroize filter.

---
+++ blocks.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define forEach(T) \
+  ^(void (^ fn)(T, int), T arr[], int n) { \
+    for (int i=0; i<n; i++) { \
+      fn(arr[i], i); \
+    } \
+  }
+
+#define map(A,B) \
+  ^(B (^fn)(A), A input[], B output[], int n) { \
+    forEach(A)( ^(A val, int i) { \
+      output[i] = fn(val); \
+    }, input, n); \
+}
+
+#define filter(T) \
+  ^(int (^ fn)(T), T arr[], T out[], int n) { \
+  int pos = 0; \
+  for (int i=0; i<n; i++) { \
+    if (fn(arr[i])) { \
+      out[pos++] = arr[i]; \
+    } \
+  } \
+  return pos; \
+}
+
+int main() {
+  int input[4] = {1,2,3,4};
+  int intermediate[4];
+  int output[4];
+  map(int,int)(^(int x) { return x+1; }, input, intermediate, 4);
+  int count = filter(int)(^int (int x) { return x%2==0;}, intermediate, output, 4);
+  forEach(int)(^(int val, int index) {
+      printf("%d ", val);
+  }, output, count);
+  printf("\n");
+}
