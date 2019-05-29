f0dd929fb63ef7e342080fa0a8e48a4859598c61
Andrew Thomson <athomsonguy@gmail.com>
2019-05-29 03:18:26 -0400

    for forEach macroized

--- nested.c
+++ nested.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define forEach(T) \
+  ({void _(void (*fn)(T, int), T arr[], int n) { \
+    for (int i=0; i<n; i++) { \
+      fn(arr[i], i); \
+    } \
+  } (void (*)(void (*)(T, int), T [], int))_;})
+
 int main() {
   int input[4] = {1,2,3,4};
   int intermediate[4];
   int output[4];
   
-  void printr(int x) {
+  void printr(int x, int idx) {
     printf("%d", x);
   }
-  ({void _(int arr[], int n) {
-    for (int i=0; i<n; i++) {
-      printf("%d", arr[i]);
-    }
-  } (void (*)(int [], int))_;})(output, 4);
+  forEach(int)(printr, output, 4);
   printf("\n");
 }
