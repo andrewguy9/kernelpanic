cc3668ec1e12f0f50be8170550d3c5af34510f26
Andrew Thomson <athomsonguy@gmail.com>
2019-05-29 11:17:55 -0700

    progress on lambda.

--- blocks.c
+++ blocks.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define lambda_inc \
+  ^ int (int x) { return x + 1; }
+
 #define forEach(T) \
   ^(void (^ fn)(T, int), T arr[], int n) { \
     for (int i=0; i<n; i++) { \
@@ -30,6 +33,8 @@ int main() {
   int input[4] = {1,2,3,4};
   int intermediate[4];
   int output[4];
+  int result = lambda_inc(1);
+  printf("%c\n", result);
   map(int,int)(^(int x) { return x+1; }, input, intermediate, 4);
   int count = filter(int)(^int (int x) { return x%2==0;}, intermediate, output, 4);
   forEach(int)(^(int val, int index) {
