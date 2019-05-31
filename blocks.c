fdd241ef5e1d6f3d9e3c96202b638f889eadcd6e
Andrew Thomson <athomsonguy@gmail.com>
2019-05-30 21:23:17 -0700

    use lambda for part of map def.

--- blocks.c
+++ blocks.c
@@ -15,7 +15,7 @@
   }
 
 #define map(A,B) \
-  ^void (B (^fn)(A), A input[], B output[], int n) { \
+  lambda(void, B (^fn)(A), A input[], B output[], int n) { \
     forEach(A)( ^(A val, int i) { \
       output[i] = fn(val); \
     }, input, n); \
