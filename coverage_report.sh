78dd723a848e838fffb6f929ddc2108af645df31
Andrew Thomson <athomsonguy@gmail.com>
2020-02-06 17:33:39 -0800

    perf_tests -> tests

--- coverage_report.sh
+++ coverage_report.sh
@@ -4,9 +4,9 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 function first { echo "$1"; }
 function rest { shift; echo "$@"; }
 
-perf_tests=( $(cat performance.txt regression.txt | grep dbg) )
-first_test=$(first "${perf_tests[@]}")
-rest_tests=( $(rest "${perf_tests[@]}") )
+tests=( $(cat performance.txt regression.txt | grep dbg) )
+first_test=$(first "${tests[@]}")
+rest_tests=( $(rest "${tests[@]}") )
 tests_str=$(join_by "," "${rest_tests[@]}")
 
 #TODO use xcrun only on Darwin.
